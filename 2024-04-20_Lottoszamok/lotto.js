e8b6af1f24e64c82c95a4217b83678c526278553
fulopzsolt1999 <fulopzsolt1999@gmail.com>
2024-04-24 10:45:16 +0200

    - Deleted unused imgs + Rewrite website look + External css + First exercise finished

--- 2024-04-20_Lottoszamok/lotto.js
+++ 2024-04-20_Lottoszamok/lotto.js
@@ -1,30 +1,247 @@
 $(document).ready(function () {
-   const LottoNumberGenerator = (szamokMennyisege, maxKihuzhatoSzam) => {
-      if (szamokMennyisege <= maxKihuzhatoSzam) {
-         let lottoSzamok = [];
-         while (lottoSzamok.length < szamokMennyisege) {
-            let generaltSzam = Math.round(Math.random() * (maxKihuzhatoSzam - 1)) + 1;
-            if (!lottoSzamok.includes(generaltSzam)) {
-               lottoSzamok.push(generaltSzam);
+   var allLotto5Numbers = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+      9: 0,
+      10: 0,
+      11: 0,
+      12: 0,
+      13: 0,
+      14: 0,
+      15: 0,
+      16: 0,
+      17: 0,
+      18: 0,
+      19: 0,
+      20: 0,
+      21: 0,
+      22: 0,
+      23: 0,
+      24: 0,
+      25: 0,
+      26: 0,
+      27: 0,
+      28: 0,
+      29: 0,
+      30: 0,
+      31: 0,
+      32: 0,
+      33: 0,
+      34: 0,
+      35: 0,
+      36: 0,
+      37: 0,
+      38: 0,
+      39: 0,
+      40: 0,
+      41: 0,
+      42: 0,
+      43: 0,
+      44: 0,
+      45: 0,
+      46: 0,
+      47: 0,
+      48: 0,
+      49: 0,
+      50: 0,
+      51: 0,
+      52: 0,
+      53: 0,
+      54: 0,
+      55: 0,
+      56: 0,
+      57: 0,
+      58: 0,
+      59: 0,
+      60: 0,
+      61: 0,
+      62: 0,
+      63: 0,
+      64: 0,
+      65: 0,
+      66: 0,
+      67: 0,
+      68: 0,
+      69: 0,
+      70: 0,
+      71: 0,
+      72: 0,
+      73: 0,
+      74: 0,
+      75: 0,
+      76: 0,
+      77: 0,
+      78: 0,
+      79: 0,
+      80: 0,
+      81: 0,
+      82: 0,
+      83: 0,
+      84: 0,
+      85: 0,
+      86: 0,
+      87: 0,
+      88: 0,
+      89: 0,
+      90: 0,
+   };
+   var allLotto6Numbers = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+      9: 0,
+      10: 0,
+      11: 0,
+      12: 0,
+      13: 0,
+      14: 0,
+      15: 0,
+      16: 0,
+      17: 0,
+      18: 0,
+      19: 0,
+      20: 0,
+      21: 0,
+      22: 0,
+      23: 0,
+      24: 0,
+      25: 0,
+      26: 0,
+      27: 0,
+      28: 0,
+      29: 0,
+      30: 0,
+      31: 0,
+      32: 0,
+      33: 0,
+      34: 0,
+      35: 0,
+      36: 0,
+      37: 0,
+      38: 0,
+      39: 0,
+      40: 0,
+      41: 0,
+      42: 0,
+      43: 0,
+      44: 0,
+      45: 0,
+   };
+   var allSkandinavNumbers = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+      9: 0,
+      10: 0,
+      11: 0,
+      12: 0,
+      13: 0,
+      14: 0,
+      15: 0,
+      16: 0,
+      17: 0,
+      18: 0,
+      19: 0,
+      20: 0,
+      21: 0,
+      22: 0,
+      23: 0,
+      24: 0,
+      25: 0,
+      26: 0,
+      27: 0,
+      28: 0,
+      29: 0,
+      30: 0,
+      31: 0,
+      32: 0,
+      33: 0,
+      34: 0,
+      35: 0,
+   };
+   const LottoNumberGenerator = (lottoType, lottoMaxNumber) => {
+      if (lottoType <= lottoMaxNumber) {
+         let lottoNumbers = [];
+         while (lottoNumbers.length < lottoType) {
+            let genNumber = Math.round(Math.random() * (lottoMaxNumber - 1)) + 1;
+            if (!lottoNumbers.includes(genNumber)) {
+               switch (Number(lottoType)) {
+                  case 5:
+                     const allLotto5NumbersKeys = Object.keys(allLotto5Numbers);
+                     for (let i = 0; i < allLotto5NumbersKeys.length; i++) {
+                        if (genNumber === Number(allLotto5NumbersKeys[i])) {
+                           allLotto5Numbers[allLotto5NumbersKeys[i]]++;
+                        }
+                     }
+                     break;
+                  case 6:
+                     const allLotto6NumbersKeys = Object.keys(allLotto6Numbers);
+                     for (let i = 0; i < allLotto6NumbersKeys.length; i++) {
+                        if (genNumber === Number(allLotto6NumbersKeys[i])) {
+                           allLotto6Numbers[allLotto6NumbersKeys[i]]++;
+                        }
+                     }
+                     break;
+                  case 7:
+                     const allSkandinavKeys = Object.keys(allSkandinavNumbers);
+                     for (let i = 0; i < allSkandinavKeys.length; i++) {
+                        if (genNumber === Number(allSkandinavKeys[i])) {
+                           allSkandinavNumbers[allSkandinavKeys[i]]++;
+                        }
+                     }
+                     break;
+
+                  default:
+                     break;
+               }
+               lottoNumbers.push(genNumber);
             }
          }
-         return lottoSzamok;
+         lottoNumbers.sort((a, b) => {
+            return a - b;
+         });
+         return lottoNumbers;
       } else {
          return "NEM megfelelő a függvény paraméterezése!";
       }
    };
 
    $("#btn-5-lotto").click(function () {
+      const lotto5Numbers = LottoNumberGenerator($("#btn-5-lotto").val(), 90);
+      const lottoType = $("#btn-5-lotto").val();
       $(".ball-container").html("");
-      GenerateBalls($("#btn-5-lotto").val(), LottoNumberGenerator($("#btn-5-lotto").val(), 90));
+      GenerateBalls(lottoType, lotto5Numbers);
+      GenerateTable(allLotto5Numbers, lottoType, lotto5Numbers);
    });
    $("#btn-6-lotto").click(function () {
+      const lotto6Numbers = LottoNumberGenerator($("#btn-6-lotto").val(), 45);
+      const lottoType = $("#btn-6-lotto").val();
       $(".ball-container").html("");
-      GenerateBalls($("#btn-6-lotto").val(), LottoNumberGenerator($("#btn-6-lotto").val(), 45));
+      GenerateBalls(lottoType, lotto6Numbers);
+      GenerateTable(allLotto6Numbers, lottoType, lotto6Numbers);
    });
    $("#btn-skandinav").click(function () {
+      const lottoSkandinavNumbers = LottoNumberGenerator($("#btn-skandinav").val(), 35);
+      const lottoType = $("#btn-skandinav").val();
       $(".ball-container").html("");
-      GenerateBalls($("#btn-skandinav").val(), LottoNumberGenerator($("#btn-skandinav").val(), 35));
+      GenerateBalls(lottoType, lottoSkandinavNumbers);
+      GenerateTable(allSkandinavNumbers, lottoType, lottoSkandinavNumbers);
    });
 
    const GenerateBalls = (ballsNum, lottoNumbers) => {
@@ -33,6 +250,78 @@ $(document).ready(function () {
             `<div class='show-ball row'><img src='img/blank_ball.png'><span id='ball-number-${i}'>${lottoNumbers[i]}</span></div>`
          );
       }
+   };
+
+   const GenerateTable = (lottoNumbersObj, lottoType, actLottoNums) => {
+      $(".table-container").html("");
+      switch (Number(lottoType)) {
+         case 5:
+            $(".table-container").append(
+               `<table class="table table-hover table-striped table-dark">
+                  <tr>
+                     <th>Vizsgálat</th>
+                     <th>5-ös Lottó Adatok</th>
+                  </tr>
+                  <tr>
+                     <th>Szám előfordulás</th>
+                     <td id="lotto-num-cell">
+                        <div class="row" id="lotto-num-stat"></div>
+                     </td>
+                  </tr>
+
+               </table>`
+            );
+            break;
+         case 6:
+            $(".table-container").append(
+               `<table class="table table-hover table-striped table-dark">
+                  <tr>
+                     <th>Vizsgálat</th>
+                     <th>6-os Lottó Adatok</th>
+                  </tr>
+                  <tr>
+                     <th>Szám előfordulás</th>
+                     <td id="lotto-num-cell">
+                        <div class="row" id="lotto-num-stat"></div>
+                     </td>
+                  </tr>
+
+               </table>`
+            );
+            break;
+         case 7:
+            $(".table-container").append(
+               `<table class="table table-hover table-striped table-dark">
+                  <tr>
+                     <th>Vizsgálat</th>
+                     <th>Skandináv Lottó Adatok</th>
+                  </tr>
+                  <tr>
+                     <th>Szám előfordulás</th>
+                     <td id="lotto-num-cell">
+                        <div class="row" id="lotto-num-stat"></div>
+                     </td>
+                  </tr>
+
+               </table>`
+            );
+            break;
+
+         default:
+            break;
+      }
+
+      Object.keys(lottoNumbersObj).forEach((key) => {
+         if (actLottoNums.includes(Number(key))) {
+            $("#lotto-num-stat").append(
+               `<div class="col-1 p-0 m-2 text-dark bg-light" id="num"><${key}: ${lottoNumbersObj[key]}> </div>`
+            );
+         } else {
+            $("#lotto-num-stat").append(
+               `<div class="col-1 p-0 m-2" id="num"><${key}: ${lottoNumbersObj[key]}> </div>`
+            );
+         }
+      });
    };
 });
 
